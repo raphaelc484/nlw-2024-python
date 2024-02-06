c31a86ea6700bb24bac9db0805db0a839e379b9f
raphaelc484 <raphaelc484@hotmail.com>
2024-02-06 14:37:16 -0300

    finish day 2

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 
 class TagCreatorView:
@@ -8,11 +9,12 @@ class TagCreatorView:
     """
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
 
         # logica de regra  de negocio
-        print("estou na minha view")
-        print(http_request)
+        formatted_response = tag_creator_controller.create(product_code)
         # returno http
-        return HttpResponse(status_code=200, body={"Hello": "World"})
+        return HttpResponse(status_code=200, body=formatted_response)
